a0b89aa882cf31371c81b2eba4c9032fe4cb0692
prncoprs <prncoprs@163.com>
2022-06-23 13:50:13 +0800

    add symbols-outline plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -95,6 +95,9 @@ return packer.startup(function(use)
   -- Git
   use "lewis6991/gitsigns.nvim"
 
+  -- More plugins
+  use "simrat39/symbols-outline.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
